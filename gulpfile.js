1a7b7b322fa80655b9866f22a7fd99d81ea4a489
Aleksandar Lazanski <117667390+LazanIT@users.noreply.github.com>
2023-10-08 18:58:42 +0200

    purge css explained

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@ const sass = require("gulp-sass")(require("sass"));
 const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("custom-library/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("custom-library/**/*.scss")
+    .pipe(sass())
+    .pipe(dest("css"))
+    .pipe(purgecss({ content: ["*.html"] }));
 }
 
 function watchTask() {
-  watch(["custom-library/**/*.scss"], buildStyles);
+  watch(["custom-library/**/*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
