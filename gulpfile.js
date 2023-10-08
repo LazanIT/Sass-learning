bc36debeb53785ebc3bf676d42f0803ed9cbe8eb
Aleksandar Lazanski <117667390+LazanIT@users.noreply.github.com>
2023-10-08 19:00:43 +0200

    Little comment explained about purgecss

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,8 @@ function watchTask() {
 }
 
 exports.default = series(buildStyles, watchTask);
+
+// Pomocu PURGE CSS mi minimiziramo trenutni css
+// Tj. da nemamo 4000 ili 5000 linija koda
+// Pomocu update gulpfile.js mi koristimo i klase koje su napravljene ali se ne prikazuju u css-u
+// Tek kada ih iskoristimo u nekom html-u to ime klase se pojavljuje
